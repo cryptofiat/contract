909cbc69982fca69e5d3ca930d7047d709ada280
Egon Elbre <egonelbre@gmail.com>
2016-10-01 00:11:46 +0300

    Add scripts to invoke solc command-line compiler.

---
+++ build.solc.bat
@@ -0,0 +1,3 @@
+rmdir /s /q .bin
+mkdir .bin
+solc -o ./.bin --bin --abi --asm --optimize --optimize-runs 1000000 --gas ./CryptoFiat.sol >> ./.bin/_gas.log
---
+++ build.solc.sh
@@ -0,0 +1,3 @@
+rm -f .bin
+mkdir .bin
+solc -o ./.bin --bin --abi --asm --optimize --optimize-runs 1000000 --gas ./CryptoFiat.sol >> ./.bin/_gas.log
